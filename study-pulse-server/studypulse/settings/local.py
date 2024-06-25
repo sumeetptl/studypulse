1d3766467d36d3b228d50edebd0e21eea869afc7
Sumeet Patil <thesumeetpatil@gmail.com>
2024-06-26 03:05:14 +0530

    Master :: Basic requirements and Settings

---
+++ study-pulse-server/studypulse/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+ALLOWED_HOSTS = ['localhost']
+DEBUG = True
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'local_db',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
---
+++ study-pulse-server/studypulse/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+import os
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': int(os.environ.get('DATABASE_PORT')),
+    }
+}
