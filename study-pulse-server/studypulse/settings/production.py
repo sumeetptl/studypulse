f0c4b22a07ed06f8b8cf70bbe7635867a2d28f17
Sumeet Patil <thesumeetpatil@gmail.com>
2024-06-26 18:22:55 +0530

    DB Update

--- study-pulse-server/studypulse/settings/production.py
+++ study-pulse-server/studypulse/settings/production.py
@@ -3,17 +3,17 @@ from .base import *
 import dj_database_url
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': int(os.environ.get('DATABASE_PORT')),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         "USER": os.environ.get("SQL_USER"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD"),
+#         'NAME': os.environ.get('DATABASE_NAME'),
+#         'HOST': os.environ.get('DATABASE_HOST'),
+#         'PORT': int(os.environ.get('DATABASE_PORT')),
+#     }
+# }
+DATABASES = {}
 # postgresql://root:uWB0oEy90tzTqPyt36CapiLp5TaTNkDG@dpg-cptveo08fa8c738rlh90-a.oregon-postgres.render.com/studypulse
 DATABASES["default"] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
