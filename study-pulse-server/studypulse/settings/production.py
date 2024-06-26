b3ad49b57892758d8aac8e5dc577061a06a70e47
Sumeet Patil <thesumeetpatil@gmail.com>
2024-06-27 01:10:06 +0530

    Docker compose development

--- study-pulse-server/studypulse/settings/production.py
+++ study-pulse-server/studypulse/settings/production.py
@@ -12,3 +12,14 @@ DATABASES = {
         'PORT': int(os.environ.get('DATABASE_PORT')),
     }
 }
+
+# DATABASES = {}
+# # postgresql://root:uWB0oEy90tzTqPyt36CapiLp5TaTNkDG@dpg-cptveo08fa8c738rlh90-a.oregon-postgres.render.com/studypulse
+# DATABASES["default"] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
+ROOT_URLCONF = 'studypulse.urls'
+
+STATIC_URL = "static/"
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
