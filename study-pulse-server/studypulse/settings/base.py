1313f1440592bb50b6d7cf159c76dc458c5366bc
Sumeet Patil <thesumeetpatil@gmail.com>
2024-06-27 21:18:17 +0530

    Allowed host blob

--- study-pulse-server/studypulse/settings/base.py
+++ study-pulse-server/studypulse/settings/base.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
