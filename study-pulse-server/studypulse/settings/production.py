3b7a4c1f5bc6785b10818145448f5172db140846
Sumeet Patil <thesumeetpatil@gmail.com>
2024-06-27 20:46:40 +0530

    Dev : Render Settings

--- study-pulse-server/studypulse/settings/production.py
+++ study-pulse-server/studypulse/settings/production.py
@@ -1,21 +1,21 @@
 import os
 from .base import *
+import dj_database_url
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         "USER": os.environ.get("SQL_USER"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD"),
+#         'NAME': os.environ.get('DATABASE_NAME'),
+#         'HOST': os.environ.get('DATABASE_HOST'),
+#         'PORT': int(os.environ.get('DATABASE_PORT')),
+#     }
+# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': int(os.environ.get('DATABASE_PORT')),
-    }
-}
-
-# DATABASES = {}
+DATABASES = {}
 # # postgresql://root:uWB0oEy90tzTqPyt36CapiLp5TaTNkDG@dpg-cptveo08fa8c738rlh90-a.oregon-postgres.render.com/studypulse
-# DATABASES["default"] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES["default"] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 ROOT_URLCONF = 'studypulse.urls'
 
