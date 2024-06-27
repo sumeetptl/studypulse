f46aa0d2faa8defd8bf8df04fde4aaf8e3ae0a8c
Sumeet Patil <thesumeetpatil@gmail.com>
2024-06-27 21:04:37 +0530

    Render-v2 Settings

--- study-pulse-server/studypulse/wsgi.py
+++ study-pulse-server/studypulse/wsgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studypulse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'studypulse.settings.production')
 
 application = get_wsgi_application()
